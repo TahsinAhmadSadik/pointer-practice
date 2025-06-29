51871f281bdd9bdd9e3ac1fef962af219b184d38
Sadik <75713333+TahsinAhmadSadik@users.noreply.github.com>
2025-06-29 16:06:12 +0600

    Updated Solution for Problem 4

--- Problem 4.c
+++ Problem 4.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <windows.h>
 
 void fillPlane(int ***a, int N, int s, int sqr)
 {
@@ -100,6 +99,38 @@ int main()
         printf("Enter plane index (1-5): ");
         scanf("%d", &n);
     }
+
+    //Free Memory
+    for(int i=0; i<N; i++)
+    {
+        for(int j=0; j<i+1; j++) free(*(*(a+i)+j));
+        free(*(a+i));
+    }
+    free(a);
+    for(int i=0; i<N; i++)
+    {
+        for(int j=0; j<i+1; j++) free(*(*(b+i)+j));
+        free(*(b+i));
+    }
+    free(b);
+    for(int i=0; i<N; i++)
+    {
+        for(int j=0; j<i+1; j++) free(*(*(c+i)+j));
+        free(*(c+i));
+    }
+    free(c);
+    for(int i=0; i<N; i++)
+    {
+        for(int j=0; j<i+1; j++) free(*(*(d+i)+j));
+        free(*(d+i));
+    }
+    free(d);
+    for(int i=0; i<N; i++)
+    {
+        for(int j=0; j<1; j++) free(*(*(e+i)+j));
+        free(*(e+i));
+    }
+    free(e);
 
     printf("Exiting program.");
 }
